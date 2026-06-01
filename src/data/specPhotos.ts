75485a86c8eef099a54b982bc4d3df73441b9578
Dominik Thompson <dominik@buildodyssey.com>
2026-06-01 12:12:37 -0600

    add photo for 209 N 4117 E

--- src/data/specPhotos.ts
+++ src/data/specPhotos.ts
@@ -16,4 +16,5 @@ export const specPhotos: Record<string, string> = {
   '5025 Eagle Creek Rd':                    '/media/quick-move-ins/5025-eagle-creek-rd.jpg',
   '5085 Eagle Creek Rd':                    '/media/quick-move-ins/5085-eagle-creek-rd.jpg',
   '5055 Eagle Creek Rd':                    '/media/quick-move-ins/5055-eagle-creek-rd.jpg',
+  '209 N 4117 E':                           '/media/quick-move-ins/209-n-4117-e.jpg',
 };
